2fc8ec71fe530dbc28e3efdf51f501f7478222d2
Artem Pavlov <artyom_pavlov@bk.ru>
2021-12-15 00:35:45 +0300

    second commit

--- OOP.py
+++ OOP.py
@@ -14,15 +14,6 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
@@ -39,6 +30,15 @@ class Reviewer(Mentor):
         self.surname = surname
         self.courses_attached = []
 
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 first_reviewer = Reviewer('Edward', 'Norton')
 first_lecturer = Lecturer('Tim', 'Smith')
@@ -50,10 +50,10 @@ cool_mentor.courses_attached += ['Python']
 first_lecturer.courses_attached += ['Python']
 first_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
+first_reviewer.rate_hw(best_student, 'Python', 10)
+first_reviewer.rate_hw(best_student, 'Python', 10)
+first_reviewer.rate_hw(best_student, 'Python', 10)
+first_reviewer.rate_hw(best_student, 'Python', 10)
 print(best_student.grades)
 print(first_reviewer.courses_attached)
 print(first_lecturer.courses_attached)
