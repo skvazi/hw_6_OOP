a5a921b2f5294ec5f1e6145b8c87bfc50d23759c
Artem Pavlov <artyom_pavlov@bk.ru>
2021-12-15 02:13:24 +0300

    4th added 4 task

--- OOP.py
+++ OOP.py
@@ -33,6 +33,16 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.lecturer_grade = {}
 
+    def __str__(self):
+        res = f'Имя: {self.name.title()}\n' \
+              f'Фамилия: {self.surname.title()}\n' \
+              f'Средняя оценка за лекции: {average_rating_lesson(lecturer_list)}'
+        return res
+
+    def average_grade(self):
+        for key in self.lecturer_grade.keys():
+            return key
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -50,21 +60,33 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        res = f'Имя: {self.name.title()}\n' \
+              f'Фамилия: {self.surname.title()}'
+        return res
+
 
 first_reviewer = Reviewer('edward', 'norton')
+second_reviewer = Reviewer('John', 'Stump')
+
 first_lecturer = Lecturer('Tim', 'smith')
+second_lecturer = Lecturer('Liza', 'Simpson')
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 new_student = Student('Nally', 'Zwan', 'female')
 new_student.courses_in_progress += ['Java']
-
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
+new_student.courses_in_progress += ['Git']
 
 first_lecturer.courses_attached += ['Java']
 first_lecturer.courses_attached += ['Python']
+second_lecturer.courses_attached += ['Java']
+second_lecturer.courses_attached += ['Python']
+
+
 first_reviewer.courses_attached += ['Python']
 first_reviewer.courses_attached += ['Java']
+second_reviewer.courses_attached += ['Git']
 
 first_reviewer.rate_hw(best_student, 'Python', 10)
 first_reviewer.rate_hw(best_student, 'Python', 10)
@@ -72,11 +94,48 @@ first_reviewer.rate_hw(best_student, 'Python', 10)
 first_reviewer.rate_hw(best_student, 'Python', 10)
 first_reviewer.rate_hw(new_student, 'Java', 5)
 first_reviewer.rate_hw(new_student, 'Java', 9)
+first_reviewer.rate_hw(new_student, 'Git', 8)
+second_reviewer.rate_hw(new_student, 'Git', 9)
 
 best_student.lecture_rate(first_lecturer, 'Python', 5)
 new_student.lecture_rate(first_lecturer, 'Java', 6)
+new_student.lecture_rate(second_lecturer, 'Git', 10)
 
-print(best_student.grades)
-print(first_reviewer.courses_attached)
-print(first_lecturer.courses_attached)
-print(first_lecturer.lecturer_grade)
+
+# print(best_student.grades)
+# print(first_reviewer.courses_attached)
+# print(first_lecturer.courses_attached)
+# print(first_lecturer.lecturer_grade)
+# print(first_reviewer.__str__())
+
+
+
+
+
+# Задание 4.Полевые испытания
+student_list = [best_student, new_student]
+lecturer_list = [first_lecturer, second_lecturer]
+
+
+def average_rating_hw(students, courses):
+    sum_course_grade = 0
+    iterator = 0
+    for student in students:
+        for key, value in student.grades.items():
+            if courses in key:
+                sum_course_grade += sum(value) / len(value)
+                iterator += 1
+    return round(sum_course_grade / iterator, 2)
+
+
+def average_rating_lesson(lecturers, courses):
+    sum_course_grade = 0
+    iterator = 0
+    for x in lecturers:
+        for key, value in x.lecturer_grade.items():
+            if courses in key:
+                sum_course_grade += sum(value) / len(value)
+                iterator += 1
+    return round(sum_course_grade / iterator, 2)
+
+print(average_rating_lesson(lecturer_list, 'Python'))
