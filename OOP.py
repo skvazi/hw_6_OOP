5e7e74051dcfca1b8575475aead014853cc91dab
Artem Pavlov <artyom_pavlov@bk.ru>
2021-12-15 01:19:21 +0300

    lecture rate method

--- OOP.py
+++ OOP.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def lecture_rate(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in first_lecturer.lecturer_grade:
+                first_lecturer.lecturer_grade[course] += grade
+            else:
+                first_lecturer.lecturer_grade[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -21,6 +31,7 @@ class Lecturer(Mentor):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        self.lecturer_grade = {}
 
 
 class Reviewer(Mentor):
@@ -40,20 +51,32 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-first_reviewer = Reviewer('Edward', 'Norton')
-first_lecturer = Lecturer('Tim', 'Smith')
+first_reviewer = Reviewer('edward', 'norton')
+first_lecturer = Lecturer('Tim', 'smith')
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+new_student = Student('Nally', 'Zwan', 'female')
+new_student.courses_in_progress += ['Java']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+
+first_lecturer.courses_attached += ['Java']
 first_lecturer.courses_attached += ['Python']
 first_reviewer.courses_attached += ['Python']
+first_reviewer.courses_attached += ['Java']
 
 first_reviewer.rate_hw(best_student, 'Python', 10)
 first_reviewer.rate_hw(best_student, 'Python', 10)
 first_reviewer.rate_hw(best_student, 'Python', 10)
 first_reviewer.rate_hw(best_student, 'Python', 10)
+first_reviewer.rate_hw(new_student, 'Java', 5)
+first_reviewer.rate_hw(new_student, 'Java', 9)
+
+best_student.lecture_rate(first_lecturer, 'Python', 5)
+new_student.lecture_rate(first_lecturer, 'Java', 6)
+
 print(best_student.grades)
 print(first_reviewer.courses_attached)
 print(first_lecturer.courses_attached)
+print(first_lecturer.lecturer_grade)
